2db84a551f94f544dd44a9175cde1bfa2f2fbf0c
Nicolas Brusco <nicolasbrusco@hotmail.com>
2023-05-14 17:29:56 -0300

    fix auth middlewares to handle jwt expiration

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,16 @@ const isProtected = (req, res, next) => {
   if (!token) {
     return res.redirect("/");
   } else {
-    jwt.verify(token, config.JWT_SECRET);
+    const decodedToken = jwt.verify(token, config.JWT_SECRET, {
+      ignoreExpiration: true,
+    });
+
+    if (Date.now() / 1000 > decodedToken.exp) {
+      res.clearCookie("jwtCookie");
+      return res.redirect("/");
+      // return res.status(401).send({ message: "Expired token." });
+    }
+
     next();
   }
 };
@@ -16,7 +25,16 @@ const checkLogged = (req, res, next) => {
   const token = req.cookies.jwtCookie;
 
   if (token) {
-    jwt.verify(token, config.JWT_SECRET);
+    const decodedToken = jwt.verify(token, config.JWT_SECRET, {
+      ignoreExpiration: true,
+    });
+
+    if (Date.now() / 1000 > decodedToken.exp) {
+      res.clearCookie("jwtCookie");
+      return res.redirect("/home");
+      // return res.status(401).send({ message: "Expired token." });
+    }
+
     return res.redirect("/home");
   } else {
     next();
