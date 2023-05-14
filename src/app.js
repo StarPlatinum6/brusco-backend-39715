f4f48207a75e394683e8ec7dc020b6251deaabb1
Nicolas Brusco <nicolasbrusco@hotmail.com>
2023-05-14 15:55:43 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,68 +1,162 @@
+import ProductManager from "./ProductManager.js";
 import express from "express";
-import handlebars from "express-handlebars";
-import morgan from "morgan";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import initializePassport from "./auth/passport.js";
-
-import database from "./db.js";
-import __dirname from "./utils.js";
-import config from "./config.js";
-import { multiply } from "./views/helpers.js";
-import socket from "./socket.js";
-
-import productsRouter from "./routes/products.router.js";
-import cartsRouter from "./routes/carts.router.js";
-import messagesRouter from "./routes/messages.router.js";
-import viewsRouter from "./routes/views.router.js";
-import sessionsRouter from "./routes/sessions.router.js";
 
 const env = async () => {
   const app = express();
+  const productManager = new ProductManager();
 
-  app.use(express.json());
+  const product1 = {
+    title: "AMD Ryzen 5600X",
+    description: "6 Core AMD Processor",
+    price: 6000,
+    thumbnail: "No image",
+    code: "AMD5600X",
+    stock: 50,
+  };
+
+  const product2 = {
+    title: "Intel Core i7 13700K",
+    description: "8 Core Intel Processor",
+    price: 8000,
+    thumbnail: "No image",
+    code: "INTEL13700K",
+    stock: 33,
+  };
+
+  const product3 = {
+    title: "AMD Ryzen 5950X",
+    description: "16 Core AMD Processor",
+    price: 16000,
+    thumbnail: "No image",
+    code: "AMD5950X",
+    stock: 10,
+  };
+
+  const product4 = {
+    title: "Intel Core i9 13900K",
+    description: "14 Core Intel Processor",
+    price: 15000,
+    thumbnail: "No image",
+    code: "INTEL13900K",
+    stock: 16,
+  };
+
+  const product5 = {
+    title: "Nvidia RTX 2060 Super",
+    description: "Mid-end Nvidia Gaming GPU",
+    price: 10000,
+    thumbnail: "No image",
+    code: "RTX2060",
+    stock: 39,
+  };
+
+  const product6 = {
+    title: "AMD Radeon 6600XT",
+    description: "Mid-end Radeon Gaming GPU",
+    price: 12000,
+    thumbnail: "No image",
+    code: "RADEON6600XT",
+    stock: 47,
+  };
+
+  const product7 = {
+    title: "Nvidia RTX 3080Ti",
+    description: "High-end Nvidia Gaming GPU",
+    price: 22000,
+    thumbnail: "No image",
+    code: "RTX3080TI",
+    stock: 28,
+  };
+
+  const product8 = {
+    title: "AMD Radeon 6950XT",
+    description: "High-end Radeon Gaming GPU",
+    price: 20000,
+    thumbnail: "No image",
+    code: "RADEON6950XT",
+    stock: 13,
+  };
+
+  const product9 = {
+    title: "Nvidia RTX 4090",
+    description: "Enthusiast Nvidia Gaming GPU",
+    price: 52000,
+    thumbnail: "No image",
+    code: "RTX4090",
+    stock: 4,
+  };
+
+  const product10 = {
+    title: "AMD Radeon 7900XT",
+    description: "Enthusiast Radeon Gaming GPU",
+    price: 45000,
+    thumbnail: "No image",
+    code: "RADEON7900XT",
+    stock: 8,
+  };
+
+  /* Uncomment for testing
+  const product11 = {
+    title: "Test product",
+  };
+  */
+
+  await productManager.addProduct(product1);
+  await productManager.addProduct(product2);
+  await productManager.addProduct(product3);
+  await productManager.addProduct(product4);
+  await productManager.addProduct(product5);
+  await productManager.addProduct(product6);
+  await productManager.addProduct(product7);
+  await productManager.addProduct(product8);
+  await productManager.addProduct(product9);
+  await productManager.addProduct(product10);
+
+  /* Uncomment for testing
+  await productManager.addProduct(product11);
+  await productManager.getProductById(50);
+  await productManager.getProductById(5);
+  await productManager.updateProduct(2,"description","This product was recently updated");
+  await productManager.getProductById(2);
+  await productManager.updateProduct(50,"description","This product was recently updated");
+  await productManager.deleteProduct(2);
+  await productManager.deleteProduct(2);
+  await productManager.deleteProduct(50);
+  */
+
+  console.log("\n | Product list:")
+  await productManager.getProducts("log")
+
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(`${__dirname}/public`));
-  app.use(morgan("dev"));
-  initializePassport();
 
-  app.use(
-    session({
-      store: MongoStore.create({
-        mongoUrl: config.DB_URL,
-        ttl: 180,
-      }),
-      resave: true,
-      saveUninitialized: false,
-      secret: config.SESSION_SECRET,
-    })
-  );
+  app.get("/products", async (req, res) => {
+    const products = await productManager.getProducts();
+    const limit = req.query.limit;
 
-  app.use("/api/products", productsRouter);
-  app.use("/api/carts", cartsRouter);
-  app.use("/api/messages", messagesRouter);
-  app.use("/api/sessions", sessionsRouter);
-  app.use("/", viewsRouter);
+    if (!limit) return res.send({ products });
 
-  app.engine(
-    "handlebars",
-    handlebars.engine({
-      helpers: {
-        multiply: multiply,
-      },
-      defaultLayout: "main",
-    })
-  );
-  app.set("view engine", "handlebars");
-  app.set("views", __dirname + "/views");
+    if (isNaN(limit)) {
+      return res.send({ Error: `Limit ${limit} is not a valid value` });
+    }
 
-  const httpServer = app.listen(8080, () =>
-    console.log("Server up in port 8080!")
-  );
+    const limitedProducts = await products.slice(0, limit);
 
-  database.connect();
+    res.send({ Products: limitedProducts });
+  });
 
-  socket.connect(httpServer);
+  app.get("/products/:pid", async (req, res) => {
+    let pid = req.params.pid;
+    const products = await productManager.getProducts();
+    const filteredProduct = await products.filter((prod) => prod.id === parseInt(pid));
+
+    if (filteredProduct == 0) {
+      res.send({ Error: `Product with ID ${pid} was not found` });
+    } else {
+      res.send({ Product: filteredProduct });
+    }
+  });
+
+  app.listen(8080, () => console.log("Server up in port 8080!"));
 };
 
 env();
