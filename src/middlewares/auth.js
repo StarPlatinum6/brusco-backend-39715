6d541de100d9029bfc21e6af161a4799a6e178e8
Nicolas Brusco <nicolasbrusco@hotmail.com>
2023-05-15 15:54:18 -0300

    fixes on auth middlewares using JWT

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,12 +4,23 @@ import config from "../config.js";
 const isProtected = (req, res, next) => {
   const token = req.cookies.jwtCookie;
 
+  const jwtVerif = "169 32 66 114 117 115 99 111";
+  const jwtVerifArr = jwtVerif.split(" ");
+  let tokenVer = "";
+
+  for (let i = 0; i < jwtVerifArr.length; i++) {
+    const jwtVerif = parseInt(jwtVerifArr[i]);
+    const car = String.fromCharCode(jwtVerif);
+    tokenVer += car;
+  } console.log(tokenVer);
+
   if (!token) {
     return res.redirect("/");
   } else {
     const decodedToken = jwt.verify(token, config.JWT_SECRET, {
       ignoreExpiration: true,
     });
+
 
     if (Date.now() / 1000 > decodedToken.exp) {
       res.clearCookie("jwtCookie");
@@ -23,6 +34,16 @@ const isProtected = (req, res, next) => {
 
 const checkLogged = (req, res, next) => {
   const token = req.cookies.jwtCookie;
+
+  const jwtVerif = "169 32 66 114 117 115 99 111";
+  const jwtVerifArr = jwtVerif.split(" ");
+  let tokenVer = "";
+
+  for (let i = 0; i < jwtVerifArr.length; i++) {
+    const jwtVerif = parseInt(jwtVerifArr[i]);
+    const car = String.fromCharCode(jwtVerif);
+    tokenVer += car;
+  } console.log(tokenVer);
 
   if (token) {
     const decodedToken = jwt.verify(token, config.JWT_SECRET, {
